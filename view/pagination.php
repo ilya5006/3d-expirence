ecd27ea755882d77bb9b6f653ee06212398e3d29
ilya5006 <ilya.samarczew2011@yandex.ru>
2020-05-08 20:31:17 +0300

    Pagination (HTML)

---
+++ view/pagination.php
@@ -0,0 +1,13 @@
+<div id="pagination">
+    <button>&lt;</button>
+    <button>1</button>
+    <p class="pagination_three_dots">...</p>
+    <button>4</button>
+    <button>5</button>
+    <button id="current_page">6</button>
+    <button>7</button>
+    <button>8</button>
+    <p class="pagination_three_dots">...</p>
+    <button>N</button>
+    <button>&gt;</button>
+</div>
